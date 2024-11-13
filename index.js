077d6c78247cbec18130dbf921694a5387c227f9
Maximillian Atherton <max@a319.co.uk>
2024-11-13 16:42:14 +0000

    Removed first .get function

--- index.js
+++ index.js
@@ -10,24 +10,18 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
-});
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
 });
 
 app.get("/api/:date?", function(req, res) {
